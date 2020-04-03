a3858ff3424908d4715a6282e9906f91632832cf
Martin Wu <wuyan@allwinnertech.com>
2020-04-03 10:46:57 +0800

    add comments for timer init

--- linux-5.4.18/drivers/clocksource/timer-probe.c
+++ linux-5.4.18/drivers/clocksource/timer-probe.c
@@ -8,20 +8,20 @@
 #include <linux/of.h>
 #include <linux/clocksource.h>
 
-extern struct of_device_id __timer_of_table[];  /* kernel image里的一个段。这个段由 TIMER_OF_DECLARE() 建立。奇怪的是，没有找到__timer_of_table的定义 */
+extern struct of_device_id __timer_of_table[];  /* kernel image里的一个段。这个段由 TIMER_OF_DECLARE() 建立 */
 
-static const struct of_device_id __timer_of_table_sentinel
+static const struct of_device_id __timer_of_table_sentinel  /* 未使用，不关心 */
 	__used __section(__timer_of_table_end);
 
 void __init timer_probe(void)
-{	/* 将设备树里的Timer与驱动里注册的Timer进行匹配，对匹配到的Timer进行初始化（调用驱动里注册的初始化函数） */
+{	/* 将设备树里的Timer与驱动里注册的Timer进行交叉匹配，调用驱动里注册的初始化函数，对匹配到的Timer进行初始化 */
 	struct device_node *np;
 	const struct of_device_id *match;
 	of_init_fn_1_ret init_func_ret;
 	unsigned timers = 0;
 	int ret;
 
-	for_each_matching_node_and_match(np, __timer_of_table, &match) {  /* 遍历驱动里注册的所有Timer */
+	for_each_matching_node_and_match(np, __timer_of_table, &match) {  /* 将设备树里的Timer与驱动里注册的Timer进行交叉匹配。每匹配到一个元素，则拿到其在驱动里注册的struct of_device_id实体match，以及对应的设备树节点np */
 		if (!of_device_is_available(np))
 			continue;
 
@@ -38,7 +38,7 @@ void __init timer_probe(void)
 		timers++;
 	}
 
-	timers += acpi_probe_device_table(timer);
+	timers += acpi_probe_device_table(timer);  /* 不关心 */
 
 	if (!timers)
 		pr_crit("%s: no matching timers found\n", __func__);
--- linux-5.4.18/include/linux/of.h
+++ linux-5.4.18/include/linux/of.h
@@ -1237,7 +1237,7 @@ static inline int of_property_read_s32(const struct device_node *np,
 #define for_each_matching_node(dn, matches) \
 	for (dn = of_find_matching_node(NULL, matches); dn; \
 	     dn = of_find_matching_node(dn, matches))
-#define for_each_matching_node_and_match(dn, matches, match) /* 从设备树的根节点开始遍历，将每个节点与matches[]里的全部元素进行匹配。匹配到的元素为match，对应的设备树节点为dn */ \
+#define for_each_matching_node_and_match(dn, matches, match) /* 从设备树的根节点开始遍历，将每个节点与matches[]里的全部元素进行匹配。匹配到一次则执行一次循环体。此次循环里匹配到的元素为match，对应的设备树节点为dn */ \
 	for (dn = of_find_matching_node_and_match(NULL, matches, match); \
 	     dn; dn = of_find_matching_node_and_match(dn, matches, match))
 
